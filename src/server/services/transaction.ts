09b8ed9de3ba237272e9f373432ce0245b8e8605
woody <woodpecker146@gmail.com>
2023-12-28 10:42:18 +0700

    feat: use CreateBankerTransactionService

--- src/server/services/transaction.ts
+++ src/server/services/transaction.ts
@@ -1,28 +1,25 @@
-import { NotFoundException } from '@roxavn/core';
 import { BaseService, inject } from '@roxavn/core/server';
 import { UserAccountNotFoundException } from '@roxavn/module-currency/base';
 import {
-  type AccountTransaction,
-  CreateTransactionService,
+  CreateBankerTransactionService,
   GetUserCurrencyAccountsService,
 } from '@roxavn/module-currency/server';
-import { GetUsersApiService } from '@roxavn/module-user/server';
+import { GetOrCreateUserService } from '@roxavn/module-user/server';
 
 import { serverModule } from '../module.js';
 import { constants } from '../../base/index.js';
 
 @serverModule.injectable()
 export class CreatePaymentTransactionService extends BaseService {
-  paymentAccountIds: Record<string, string> = {};
   paymentUserId?: string;
 
   constructor(
-    @inject(CreateTransactionService)
-    protected createTransactionService: CreateTransactionService,
-    @inject(GetUsersApiService)
-    protected getUsersApiService: GetUsersApiService,
+    @inject(CreateBankerTransactionService)
+    protected createBankerTransactionService: CreateBankerTransactionService,
     @inject(GetUserCurrencyAccountsService)
-    protected getUserCurrencyAccountsService: GetUserCurrencyAccountsService
+    protected getUserCurrencyAccountsService: GetUserCurrencyAccountsService,
+    @inject(GetOrCreateUserService)
+    protected getOrCreateUserService: GetOrCreateUserService
   ) {
     super();
   }
@@ -37,28 +34,11 @@ export class CreatePaymentTransactionService extends BaseService {
       amount: number | bigint;
     };
   }) {
-    let paymentAccountId = this.paymentAccountIds[request.currencyId];
-    if (!paymentAccountId) {
-      if (!this.paymentUserId) {
-        const { items } = await this.getUsersApiService.handle({
-          username: constants.PAYMENT_ACCOUNT,
-        });
-        if (items.length) {
-          this.paymentUserId = items[0].id;
-        } else {
-          throw new NotFoundException();
-        }
-      }
-      const { items } = await this.getUserCurrencyAccountsService.handle({
-        accounts: [{ userId: this.paymentUserId }],
-        currencyId: request.currencyId,
+    if (!this.paymentUserId) {
+      const paymentUser = await this.getOrCreateUserService.handle({
+        username: constants.PAYMENT_ACCOUNT,
       });
-      if (items.length) {
-        paymentAccountId = items[0].id;
-        this.paymentAccountIds[request.currencyId] = paymentAccountId;
-      } else {
-        throw new UserAccountNotFoundException(this.paymentUserId);
-      }
+      this.paymentUserId = paymentUser.id;
     }
 
     const { items } = await this.getUserCurrencyAccountsService.handle({
@@ -70,25 +50,16 @@ export class CreatePaymentTransactionService extends BaseService {
     }
     const userAccount = items[0];
 
-    const transactions = await this.createTransactionService.handle({
+    return this.createBankerTransactionService.handle({
+      bankerUserId: this.paymentUserId,
       currencyId: request.currencyId,
-      type: request.type,
-      originalTransactionId: request.originalTransactionId,
       metadata: request.metadata,
-      accounts: [
-        {
-          accountId: userAccount.id,
-          amount: request.account.amount,
-        },
-        {
-          accountId: paymentAccountId,
-          amount: -request.account.amount,
-        },
-      ],
+      originalTransactionId: request.originalTransactionId,
+      type: request.type,
+      account: {
+        accountId: userAccount.id,
+        amount: request.account.amount,
+      },
     });
-
-    return transactions.find(
-      (t) => t.accountId === userAccount.id
-    ) as AccountTransaction;
   }
 }
